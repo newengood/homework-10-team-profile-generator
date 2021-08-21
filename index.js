f1b0e66d28a01932475f7b803f3d496dae0044be
Taylor Anderson <taylorranderson@gmail.com>
2021-08-21 11:25:28 -0700

    updated prompts into functions

--- index.js
+++ index.js
@@ -2,88 +2,114 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateHTML = require("./src/template")
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'teamManagerName',
-      message: 'What is the team managers name?',
-    },
-    {
-      type: 'input',
-      name: 'managerID',
-      message: 'What is the managers employee ID?',
-    },
-    {
-      type: 'input',
-      name: 'managerEmail',
-      message: 'What is the managers email address?',
-    },
-    {
-      type: 'input',
-      name: 'managerOfficeNumber',
-      message: 'What is the manager office number?',
-    },
-    {
-      type: 'list',
-      name: 'employeeType',
-      message: 'Add employee or exit',
-      choices: ["Engineer", "Intern", "Finish and exit"],
-    },
-    // engineer prompts
-    {
+// create staff array
+let staff = []
+
+// add a mangager prompts
+const addManager = () => {
+    return inquirer.prompt([
+      {
         type: 'input',
-        name: 'engineerName',
-        message: 'What is the Engineers name?',
-        when: (input) => input.employeeType === "Engineer",
+        name: 'teamManagerName',
+        message: 'What is the team managers name?',
       },
       {
         type: 'input',
-        name: 'engineerID',
-        message: 'What is the Engineers ID?',
-        when: (input) => input.employeeType === "Engineer",
+        name: 'managerID',
+        message: 'What is the managers employee ID?',
       },
       {
         type: 'input',
-        name: 'engineerEmail',
-        message: 'What is the Engineers email?',
-        when: (input) => input.employeeType === "Engineer",
+        name: 'managerEmail',
+        message: 'What is the managers email address?',
       },
       {
         type: 'input',
-        name: 'engineerGithub',
+        name: 'managerOfficeNumber',
+        message: 'What is the manager office number?',
+      },
+    ])
+    .then(answers => {
+        const {teamManagerName, managerID, managerEmail, managerOfficeNumber} = answers;
+        const manager = new Manager (teamManagerName, managerID, managerEmail, managerOfficeNumber)
+        // add manager to staff
+        staff.push(manager);
+    })
+};
+
+// employee prompts
+const addEmployee = () => {
+    return inquirer.prompt([
+        {
+        type: 'list',
+        name: 'employeeType',
+        message: 'What type of employee are you adding?',
+        choices: ['Engineer', 'Intern'],
+        },
+        {
+        type: 'input',
+        name: 'employeeName',
+        message: 'What is the employees name?',
+        },
+        {
+        type: 'input',
+        name: 'employeeID',
+        message: 'What is the employees ID?',
+        },
+        {
+        type: 'input',
+        name: 'employeeEmail',
+        message: 'What is the employees email?',
+        },
+        {
+        type: 'input',
+        name: 'employeeGithub',
         message: 'What is Engineers Github username?',
         when: (input) => input.employeeType === "Engineer",
-      },
-      // Intern Prompts
-      {
+        },
+        {
         type: 'input',
-        name: 'internName',
-        message: 'What is the Interns name?',
+        name: 'employeeSchool',
+        message: 'What school does the intern attend?',
         when: (input) => input.employeeType === "Intern",
+        },
+        {
+        type: 'confirm',
+        name: 'addAdditionalEmployee',
+        message: 'Add addition employee?',
+        },
+    ])
+    .then(answers => {
+        const {employeeName, employeeID, employeeEmail, employeeGithub, employeeSchool, addAdditionalEmployee} = answers;
 
-      },
-      {
-        type: 'input',
-        name: 'internID',
-        message: 'What is the Interns ID?',
-        when: (input) => input.employeeType === "Intern",
-      },
-      {
-        type: 'input',
-        name: 'internEmail',
-        message: 'What is the Interns email?',
-        when: (input) => input.employeeType === "Intern",
-      },
-      {
-        type: 'input',
-        name: 'internSchool',
-        message: 'What school does the Intern attend?',
-        when: (input) => input.employeeType === "Intern",
-      },
-  ])
-  .then((answers) => {
-    const htmlPageContent = generateHTML(answers);
+        // add engineer to staff
+        if (answers.employeeType === "Engineer") {
+            const engineer = new Engineer (employeeName, employeeID, employeeEmail, employeeGithub);
+            staff.push(engineer);
+        // add intern to staff
+        } else if (answers.employeeType === "Intern") {
+            const intern = new Intern (employeeName, employeeID, employeeEmail, employeeSchool);
+            staff.push(intern);
+        }
+        // add additional employee if chosen
+        if (answers.addAdditionEmployee) {
+            addEmployee(staff);
+        // return completed staff if chosen
+        } else {
+            return staff;
+        }
+    });
+};
+
+// start application
+addManager()
+
+// add employees
+.then(addEmployee)
+
+// create html
+.then((staff) => {
+    const htmlPageContent = generateHTML(staff);
 
     fs.writeFile('./dist/index.html', htmlPageContent, (err) =>
       err ? console.log(err) : console.log('Successfully created index.html!')
